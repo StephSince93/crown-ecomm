d84acb3b2956993fdbef10f2339c160886a2b32c
Stephen Romero <stephen.romero@hyliion.com>
2023-11-20 16:03:41 -0600

    Created reusable button component with proper styling, imported button into sogin up component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -4,6 +4,8 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils.js";
 import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component.jsx";
+import "./sign-up-form.styles.scss";
 const defaultFormFields = {
   displayName: "",
   email: "",
@@ -50,8 +52,9 @@ const SignUpForm = () => {
     console.log("Here!");
   };
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className="sign-up-container">
+    <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="Display Name"
@@ -85,7 +88,7 @@ const SignUpForm = () => {
           name="confirmPassword"
           value={confirmPassword}
         />
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   );
