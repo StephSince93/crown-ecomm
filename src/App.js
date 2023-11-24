1894805ca01ae4e010ed83b808adfe4afd161398
Stephen Romero <stephen.romero@hyliion.com>
2023-11-24 14:31:07 -0600

    Added sample shop data with shop context, created shop card and container to display the products

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
+import Shop from "./routes/shop/shop.component";
 import Navigation from "./routes/Navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
