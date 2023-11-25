48ecfd9c8029f2a7561742bd56026cae3c673cfd
Stephen Romero <stephen.romero@hyliion.com>
2023-11-25 14:47:19 -0600

    Implemented styling for checkout page and dynamic updating when clicking on add products, added counter to cartitem

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,16 +1,43 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+const addCartItem = (cartItems, productToAdd) => {
+  console.log(cartItems, productToAdd);
+  const existingProduct = cartItems.findIndex((c) => c.id === productToAdd.id);
+  console.log(existingProduct);
+  if (existingProduct >= 0) {
+    cartItems[existingProduct].quantity += 1;
+    return [...cartItems];
+  } else {
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
+  }
+};
 
 // as the actual value you want to access
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [], // { name: '', quantity: ''}
+  addItemToCart: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
 
-  return (
-    <CartContext.Provider value={value}>{children}</CartContext.Provider>
-  );
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount };
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
