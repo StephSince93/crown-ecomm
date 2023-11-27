fe98e3cb26b7c760792b4d599f3ba3bcfc459cf6
Stephen Romero <stephen.romero@hyliion.com>
2023-11-27 13:19:06 -0600

    Fixed navigation folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 import Shop from "./routes/shop/shop.component";
-import Navigation from "./routes/navigation/navigation.component";
+import Navigation from "./routes/Navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Checkout from "./routes/checkout/checkout.component";
 const App = () => {
