c632711a69648a839599584601b751c1f61932aa
Stephen Romero <stephen.romero93@gmail.com>
2024-02-07 14:16:43 -0600

    Converted categories context to redux flow, imported redux into shop parent component, added reselect component for memoization on redux selector

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 import Category from "../category/category.component";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { setCategories } from "../../store/categories/category.action";
+
 const Shop = () => {
+  const dispatch = useDispatch();
+  const [isLoaded, load] = useState(false); //Routes loading before call is made, needed async return for this reason
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoriesArray = await getCategoriesAndDocuments("categories");
+      dispatch(setCategories(categoriesArray));
+      load(true);
+    };
+    getCategoriesMap();
+  }, []);
+
   return (
-    <Routes>
-      <Route index element={<CategoriesPreview />} />
-      <Route path=":category" element={<Category />} />
-    </Routes>
+    <>
+      {isLoaded && (
+        <Routes>
+          <Route index element={<CategoriesPreview />} />
+          <Route path=":category" element={<Category />} />
+        </Routes>
+      )}
+    </>
   );
 };
 
