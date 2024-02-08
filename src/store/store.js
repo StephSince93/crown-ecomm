ac48bcdf1c1c39a7ed042e9b1a4b96f31d821cdb
Stephen Romero <stephen.romero93@gmail.com>
2024-02-08 08:11:12 -0600

    Moved redux logger into separate middleware directory, implemented redux devtools and updated store init

--- src/store/store.js
+++ src/store/store.js
@@ -1,36 +1,34 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 // import logger from 'redux-logger';
-
-import { rootReducer } from './root-reducer';
-
-const loggerMiddleware = (store) => (next) => (action) => {
-    if(!action.type) {
-        return next(action);
-    }
-
-    console.log('type', action.type);
-    console.log('payload', action.payload);
-    console.log('currentState: ', store.getState());
-
-    next(action);
-
-    console.log('next state ', store.getState());
-}
+import { loggerMiddleware } from "./middleware/logger"; // Custom logger
+import { rootReducer } from "./root-reducer";
 
 const persistConfig = {
-    key: 'root',
-    storage,
-    blacklist: ['user']
-}
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
+const middleWares = [
+  process.env.NODE_ENV !== "production" && loggerMiddleware,
+].filter(Boolean);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
-export const store = createStore(persistedReducer, undefined, composedEnhancers);
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
 
 export const persistor = persistStore(store);
