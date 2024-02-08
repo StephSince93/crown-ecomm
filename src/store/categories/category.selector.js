bc2d779cec85586258d01fb63fc56380f7e1da2a
Stephen Romero <stephen.romero93@gmail.com>
2024-02-08 10:01:10 -0600

    implemented redux thunk for async side effects, added loading spinner and connected to category and category preview components

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -10,10 +10,17 @@ export const selectCategories = createSelector(
 export const selectCategoriesMap = createSelector(
   [selectCategories],
   (categories) => {
-    return categories.reduce((acc, category) => {
-      const { title, items } = category;
-      acc[title.toLowerCase()] = items;
-      return acc;
-    }, {});
+    return Object.keys(categories).length
+      ? categories.reduce((acc, category) => {
+          const { title, items } = category;
+          acc[title.toLowerCase()] = items;
+          return acc;
+        }, {})
+      : {};
   }
+);
+
+export const selectCategoriesIsLoading = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.isLoading
 );
